39c7bcc24267cb54d377674fe8f0af5490cf9e76
madebyphi <bawoni.phi@gmail.com>
2023-11-06 11:46:25 +0100

    Project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { sepolia } from "wagmi/chains";
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const alchemyId = process.env.ALCHEMY_API_KEY;
+const walletConnectProjectId = process.env.WALLETCONNECT_PROJECT_ID;
+
+const chains = [sepolia];
+
+const config = createConfig(
+  getDefaultConfig({
+    alchemyId,
+    walletConnectProjectId: walletConnectProjectId || "default",
+    chains,
+    appName: "TipJar",
+  })
+);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
+
+export default MyApp;
